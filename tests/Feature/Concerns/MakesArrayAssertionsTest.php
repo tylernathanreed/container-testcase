b5aafa872f796b34232b30058faa013a3c604a5c
Tyler <tylernathanreed@gmail.com>
2023-06-23 17:24:54 -0500

    ~ Disable on bad setups

--- tests/Feature/Concerns/MakesArrayAssertionsTest.php
+++ tests/Feature/Concerns/MakesArrayAssertionsTest.php
@@ -1,5 +1,24 @@
 <?php
 
+use Composer\InstalledVersions as Composer;
+
+beforeEach()->skip(function () {
+    // There is currently a bug in Laravel 9.x, where if you are
+    // using PHPUnit 10.x, the ArraySubset constraint breaks.
+    // If you're on PHPUnit 9.x or Laravel 10.x, it works.
+
+    $laravel = Composer::isInstalled('laravel/framework')
+        ? Composer::getVersion('laravel/framework')
+        : Composer::getVersion('illuminate/testing');
+
+    $phpunit = Composer::getVersion('phpunit/phpunit');
+
+    $isLaravel9  = explode('.', $laravel)[0] == 9;
+    $isPhpUnit10 = explode('.', $phpunit)[0] == 10;
+
+    return $isLaravel9 && $isPhpUnit10;
+}, 'Feature unavailable on Laravel 9.x w/ PHPUnit 10.x');
+
 it('can make array assertions with phpunit style', function () {
     $expected = [
         'expected' => 'foo',
